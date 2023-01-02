f035d18f586eb24c384395f979e4bc8b5f290ee5
tomoya <t.trance0208@gmail.com>
2023-01-02 19:00:40 +0900

    edit ajax-lesson

---
+++ Ajax-Lesson/js/script.js
@@ -0,0 +1,20 @@
+const API_KEY = "a2154396b43e25918fbb2049e7f08a10";
+
+$(function(){
+  $('#btn').on('click', function() {
+    $.ajax({
+      url: "https://api.openweathermap.org/data/2.5/weather?q=" + $('#cityname').val() + "&units=metric&appid=" + API_KEY,
+      dataType : 'jsonp',
+    }).done(function (data){
+      $('#place').text(data.name);
+      $('#temp_max').text(data.main.temp_max);
+      $('#temp_min').text(data.main.temp_min);
+      $('#humidity').text(data.main.humidity);
+      $('#speed').text(data.wind.speed);
+      $('#weather').text(data.weather[0].main);
+      $('img').attr("src","http://openweathermap.org/img/w/" + data.weather[0].icon + ".png");
+      $('img').attr("alt",data.weather[0].main);
+    }).fail(function (data) {
+      alert('通信に失敗しました。');      });
+  });
+});
